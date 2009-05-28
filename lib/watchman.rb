3ec0660e15593955b2291b47164374fe0302da48
vagmi <vagmi.mudumbai@gmail.com>
2009-05-28 19:27:08 +0530

    set up test infrastructure

--- lib/watchman.rb
+++ lib/watchman.rb
@@ -1,1 +1,8 @@
-# Watchman
+require 'watchmen_routes.rb'
+
+%w{ models controllers helpers } do |dir|
+  path = File.join(File.dirname(__FILE__),"app",dir)
+  $LOAD_PATH << path
+  ActiveSupport::Dependencies.load_paths.add(path)
+  ActiveSupport::Dependencies.load_once_path.delete(path)
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,15 @@
+ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + "/../../../.."
+
+
 require 'rubygems'
 require 'active_support'
 require 'active_support/test_case'
+require File.expand_path(File.join(ENV['RAILS_ROOT'],'config/environment.rb'))
+
+def load_schema
+  config = YAML::load(IO.read(File.dirname(__FILE__) + "/database.yml"))
+  ActiveRecord::Base.establish_connection(config[:sqlite3])
+  load(File.dirname(__FILE__) + "/test_schema.rb")
+  require File.dirname(__FILE__) + "/../rails/init.rb"
+end
