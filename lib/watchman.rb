8fef3e8b3ef859ee1bb07cbe857b34f016a1597f
vagmi <vagmi.mudumbai@gmail.com>
2009-06-05 19:07:02 +0530

    make it work with rails 2.3 engines

--- lib/watchman.rb
+++ lib/watchman.rb
@@ -6,9 +6,3 @@ require 'authlogic'
 require 'watchman/watchman_errors.rb'
 require 'watchman/extensions.rb'
 
-%w{ models controllers helpers }.each do |dir|
-  path = File.join(File.dirname(__FILE__),"app",dir)
-  $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
-end
